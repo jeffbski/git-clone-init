20157eb5a6b239a0bd257fde43545267186d0c72
Jeff Barczewski <jeff.barczewski@gmail.com>
2015-04-14 10:22:19 -0500

    initial commit

---
+++ git-clone-init.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# usage: git-clone-init base-repo project-name
+
+baseRepo="$1"
+projectName="$2"
+
+# params are required, exit if empty
+if [ -z "$baseRepo" ] || [ -z "$projectName" ]; then
+    echo "usage: git-clone-init base-repo project-name"
+    exit 1
+fi
+
+# exit on any error
+set -e
+
+git clone "$baseRepo" "$projectName"
+cd "$projectName"
+rm -rf .git
+git init
+# replace REPO with projectName in a few files
+sed -i'.orig' "s/REPO/$projectName/g" package.json README.md
+rm package.json.orig README.md.orig
+git add .
+git commit -m "Initial commit"
+
+echo "$projectName created"
